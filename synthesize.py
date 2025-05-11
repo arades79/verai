6514c9d9d5b5a5fa931f117fd16f20687e8d8fa9
S Craver <contact+git@arades.dev>
2025-05-11 10:19:09 -0400

    fix ipresynth escaping use correct dataset format

--- synthesize.py
+++ synthesize.py
@@ -7,6 +7,7 @@ from argparse import ArgumentParser
 import json
 import multiprocessing
 
+
 def make_training_case(verilog_file: Path, case_id: str = ""):
     """
     Synthesize a Verilog file using Yosys, then create a training case out of presinthesized / postsynthesized pair.
@@ -17,7 +18,7 @@ def make_training_case(verilog_file: Path, case_id: str = ""):
 
     if not case_id:
         case_id = verilog_file.parent.stem
-    
+
     output_file = Path("synthesized", verilog_file.stem).with_suffix(".synth.v")
     output_file.parent.mkdir(parents=True, exist_ok=True)
 
@@ -27,18 +28,25 @@ def make_training_case(verilog_file: Path, case_id: str = ""):
     ys.run_pass(f"write_verilog {output_file}", design)
     print(f"Synthesis complete. Output written to {output_file}")
 
-    synth_out = (output_file.read_text()
-        .replace("\\", "\\\\")
-        .replace("\n", "\\n")
-        .replace("\"", "\\\"")
-        .replace("\r", "\\r")
-        .replace("\t", "\\t")
-        .replace("\b", "\\b")
-        .replace("\f", "\\f")
-    )
+    def escape(s: str) -> str:
+        return (
+            s.replace("\\", "\\\\")
+            .replace("\n", "\\n")
+            .replace('"', '\\"')
+            .replace("\r", "\\r")
+            .replace("\t", "\\t")
+            .replace("\b", "\\b")
+            .replace("\f", "\\f")
+        )
 
-    training_case = {"question": synth_out, "answer": verilog_file.read_text()}
-    training_file = Path("training", f"{case_id}_{verilog_file.stem}").with_suffix(".json")
+    pre_synth = escape(verilog_file.read_text())
+
+    synth_out = escape(output_file.read_text())
+
+    training_case = {"question": synth_out, "answer": pre_synth}
+    training_file = Path("training", f"{case_id}_{verilog_file.stem}").with_suffix(
+        ".json"
+    )
     training_file.write_text(json.dumps(training_case))
 
 
@@ -68,18 +76,28 @@ def make_training_cases(input_dir: Path, case_id: str = ""):
             pool.join()
         #     pool.close()
         #     pool.join()
-            # try:
-            #     pool.make_training_case(verilog_file, case_id)
-            # except:
-            #     print(f"Error processing {verilog_file}. Skipping.")
-            # else:
-            #     print(f"Training case created for {verilog_file}")
+        # try:
+        #     pool.make_training_case(verilog_file, case_id)
+        # except:
+        #     print(f"Error processing {verilog_file}. Skipping.")
+        # else:
+        #     print(f"Training case created for {verilog_file}")
+
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Synthesize Verilog files and create training cases.")
-    parser.add_argument("input_dir", type=Path, help="Path to the input directory containing Verilog files.")
-    parser.add_argument("-i","--case-id", type=str, help="Case ID for the training cases.")
+    parser = ArgumentParser(
+        description="Synthesize Verilog files and create training cases."
+    )
+    parser.add_argument(
+        "input_dir",
+        type=Path,
+        help="Path to the input directory containing Verilog files.",
+    )
+    parser.add_argument(
+        "-i", "--case-id", type=str, help="Case ID for the training cases."
+    )
     args = parser.parse_args()
-    make_training_cases(args.input_dir, args.case_id) if args.case_id else make_training_cases(args.input_dir)
+    make_training_cases(
+        args.input_dir, args.case_id
+    ) if args.case_id else make_training_cases(args.input_dir)
     print(f"Training cases created in {Path('training').absolute()}")
-
